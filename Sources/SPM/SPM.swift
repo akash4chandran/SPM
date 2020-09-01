999848ace1fef54f031b95c44ef19bed14955205
“Akash” <Akash@breezeware.net>
2020-09-01 13:47:22 +0530

    Intial commit

---
+++ Sources/SPM/SPM.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Akashchellakumar on 01/09/20.
+//
+
+import Foundation
+
+public struct SPM {
+    
+    public static func printSomething(){
+        print("Hi This is first SPM")
+    }
+}
